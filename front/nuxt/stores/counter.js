25c82a5c348feba92445d46780eb76de1177d784
DGrazz <mariogalo2008@gmail.com>
2024-03-15 11:09:16 +0100

    make cart count function and styles to show count in cartIcon

--- front/nuxt/stores/counter.js
+++ front/nuxt/stores/counter.js
@@ -3,17 +3,20 @@ import { defineStore } from "pinia";
 export const useCartStore = defineStore("cart", {
     state: () => ({
         cart: [],
+        cartCount: 0,
         mostrarCartModal: false,
     }),
     actions: {
         addToCart(item) {
             const existingItem = this.cart.find((i) => i.id === item.id);
-
+            
             if (existingItem) {
                 existingItem.quantity++;
             } else {
                 this.cart.push({ ...item, quantity: 1 });
             }
+            
+            this.cartCount++;
         },
         removeFromCart(itemId) {
             const index = this.cart.findIndex((item) => item.id === itemId);
