903a9e2ebe3d062ee3d8fea7b0d26261e67dffc2
a22ivbs <a22ivabarsan@inspedralbes.cat>
2024-04-10 08:25:02 +0200

    Update API endpoint URL to use HTTPS in tienda.vue

--- front/nuxt/pages/tienda.vue
+++ front/nuxt/pages/tienda.vue
@@ -42,7 +42,7 @@ export default {
   async mounted() {
     this.mostrarModalLoader = true;
     try {
-      const response = await axios.get('https://mercedex.daw.inspedralbes.cat/back/laravel/public/api/products');
+      const response = await axios.get('http://mercedex.daw.inspedralbes.cat/back/laravel/public/api/products');
       this.productos = response.data.products;
       console.log("Version actualizadadaaadadaa")
       console.log("Esta es la respuesta", this.productos);
