e0b49bc9817765422d00f95016732b1f97e36164
a22ivbs <a22ivabarsan@inspedralbes.cat>
2024-04-10 08:38:11 +0200

    canv

--- front/nuxt/pages/tienda.vue
+++ front/nuxt/pages/tienda.vue
@@ -42,6 +42,7 @@ export default {
   async mounted() {
     this.mostrarModalLoader = true;
     try {
+      
       const response = await axios.get('http://mercedex.daw.inspedralbes.cat/back/laravel/public/api/products');
       this.productos = response.data.products;
       console.log("Version actualizadadaaadadaa")
