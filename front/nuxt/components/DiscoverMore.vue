a05a234c7d8003c849d200888acc5fbd003eb701
a16margarlop <a16margarlop@inspedralbes.cat>
2024-04-11 11:52:33 +0200

    Update DiscoverMore and Hero components

--- front/nuxt/components/DiscoverMore.vue
+++ front/nuxt/components/DiscoverMore.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="w-full flex flex-col justify-center items-center gap-4 mb-10">
+    <div class="w-full flex flex-col justify-center items-center gap-4">
         <h2 class="text-4xl font-semibold text-black">¿Quieres conocer más?</h2>
         <div class="w-[90%] h-[800px] grid grid-cols-3 gap-4">
-            <div class="relative rounded-md overflow-hidden">
+            <div class="h-full relative rounded-md overflow-hidden">
                 <img class="w-full h-full object-cover brightness-75"
                     src="https://cflvdg.avoz.es/sc/imlGZ3AQzKC3vWRvTL8_Uaiw4pc=/1280x/2021/02/11/00121613072762898436844/Foto/j29e1016.jpg"
                     alt="">
@@ -11,8 +11,8 @@
                         <p class="text-sm">Tambien, para afrontar trayectos largos</p>
                     </div>
             </div>
-            <div class="flex flex-col gap-4">
-                <div class="h-1/2 relative rounded-md overflow-hidden">
+            <div class="flex flex-col gap-4 overflow-hidden">
+                <div class="h-full relative rounded-md overflow-hidden">
                     <img class="w-full h-full object-cover brightness-75"
                         src="https://www.auto-revista.com/images/showid2/5933499?w=900&mh=700" alt="">
                     <div class="w-3/4 absolute bottom-5 left-5">
@@ -20,7 +20,7 @@
                         <p class="text-sm">En un click llevate tu Mercedes nuevo con entrega inmediata</p>
                     </div>
                 </div>
-                <div class="h-1/2 relative rounded-md overflow-hidden">
+                <div class="h-full relative rounded-md overflow-hidden">
                     <img class="w-full h-full object-cover brightness-75" src="https://www.mercedes-benz.es/content/spain/es/passengercars/services/collection/_jcr_content/root/responsivegrid/text_media_module/image.component.damq6.3329867537239.jpg/collection-mb-300sl-14.jpg" alt="">
                     <div class="w-3/4 absolute bottom-5 left-5">
                         <h3 class="text-2xl font-semibold">Mercedes ahora en tamaño de bolsillo</h3>
---
+++ front/nuxt/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-full bg-gray-800 flex justify-around items-center py-2">
+        <h2 class="font-semibold text-2xl">Mercedex</h2>
+        <div class="flex gap-4 [&>a>svg]:size-5 [&>a>svg:hover]:scale-125 [&>a>svg:hover]:-translate-y-1 [&>a>svg]:transition [&>a>svg]:duration-200 [&>a>svg]:ease-in-out">
+            <NuxtLink to="https://www.facebook.com/MercedesBenzEspana" target="_blank"><FacebookIcon/></NuxtLink>
+            <NuxtLink to="https://twitter.com/MBenzEspana" target="_blank"><TwitterIcon/></NuxtLink>
+            <NuxtLink to="https://www.youtube.com/user/MercedesBenzSpain" target="_blank"><YoutubeIcon/></NuxtLink>
+            <NuxtLink to="https://www.instagram.com/mbenzespana/" target="_blank"><InstagramIcon class="text-white"/></NuxtLink>
+            
+            
+            
+        </div>
+    </div>
+    <p class="bg-black text-white text-sm text-center py-1">© 2024 Mercedex-Benz AG. Reservados todos los derechos
+        (proveedor)</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
